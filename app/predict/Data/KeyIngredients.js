95f4047be2c6abf36d736eaf7647eed2342b23a4
ArinBisa123 <leejindairy06@gmail.com>
2025-06-10 22:58:28 +0700

    feat: key ingredients static data

---
+++ app/predict/Data/KeyIngredients.js
@@ -0,0 +1,25 @@
+const SKIN_CARE_INGREDIENTS = {
+  Normal: [
+    "Hyaluronic Acid - Melembapkan kulit secara mendalam",
+    "Vitamin C - Antioksidan untuk mencerahkan kulit",
+    "Niacinamide - Mengontrol produksi minyak dan mengecilkan pori",
+    "Ceramides - Memperkuat skin barrier",
+    "Retinol - Anti-aging dan regenerasi sel kulit",
+  ],
+  Kering: [
+    "Hyaluronic Acid - Menarik dan mengikat kelembapan",
+    "Ceramides - Memperbaiki skin barrier yang rusak",
+    "Glycerin - Humektan alami untuk melembapkan",
+    "Squalane - Emolien ringan yang mudah diserap",
+    "Peptides - Merangsang produksi kolagen",
+  ],
+  Berminyak: [
+    "Salicylic Acid - Membersihkan pori-pori yang tersumbat",
+    "Niacinamide - Mengontrol produksi sebum berlebih",
+    "Tea Tree Oil - Antibakteri dan anti-inflamasi",
+    "Zinc Oxide - Menenangkan kulit berminyak",
+    "Clay Minerals - Menyerap minyak berlebih",
+  ],
+};
+
+export { SKIN_CARE_INGREDIENTS };
